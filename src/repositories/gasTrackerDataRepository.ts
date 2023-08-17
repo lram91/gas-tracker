6e6133a4ed83e5f6b04dd5f30d40b1ffcf6c46d6
lukas9590 <k.bautronyte@gmail.com>
2023-08-17 20:04:54 +0300

    add GasTrackerData type

--- src/repositories/gasTrackerDataRepository.ts
+++ src/repositories/gasTrackerDataRepository.ts
@@ -1,8 +1,8 @@
 import { useFetchData } from '@/repositories/baseRepository';
 import { BlockchainNetworkEnum } from "@/types/enums/BlockchainNetworkEnum";
-
+import { GasTrackerData } from "@/types/storeDataTypes";
 export function useGasTrackerDataRepository() {
-    const loadGasTrackerData = async (network: BlockchainNetworkEnum): Promise<any | null> => {
+    const loadGasTrackerData = async (network: BlockchainNetworkEnum): Promise<GasTrackerData  | null> => {
         const apiUrl = `/data/${network}/gas-tracker-data.json`;
 
         return await useFetchData().fetchData(apiUrl);
